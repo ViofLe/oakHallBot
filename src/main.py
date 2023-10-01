e22d8ee5bd2a7a5a1a2f5acc3237376385216263
viofle <levionyimadu@gmail.com>
2023-10-01 18:39:07 -0400

    new file:   .vscode/settings.json 	new file:   .vscode/vex_project_settings.json 	new file:   src/main.py
    commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       levio                                                        #
+# 	Created:      10/1/2023, 6:28:24 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
